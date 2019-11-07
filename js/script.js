43c027e9efcac01170437ad80b9c8f2a96470ce0
JosephPerez13 <joseph.perez013@gmail.com>
2019-11-07 14:11:26 -0800

    htmlSites

--- js/script.js
+++ js/script.js
@@ -1,27 +1,32 @@
 let triviaQ = [];
 let totalQuestions = [20];
 
-
 function loadQuestions() {
     let xmlhttp = new XMLHttpRequest();
     let url = "";
 
-    if (diff == "easy") {
+    if (diff == "esQ") {
         url = "./data/data.json";
     }
     // This is our fail safe-----------------------------
-    if (diff == "") {
+    if (diff == "haQ") {
         url = "./data/data.json";
     }
     //-------------------------------------------
 
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            let myArr = JSON.parse(this.responseText);
+            let myArr = this.responseText //JSON.parse(this.responseText);
             allQuestions(myArr);
         }
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
 }
+
+function allQuestions(info){
+    console.log(e);
+}
+
+
 loadQuestions();
