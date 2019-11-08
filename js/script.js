2a627b9207b429ecfa111e6a4c4c154116011af6
JosephPerez13 <joseph.perez013@gmail.com>
2019-11-07 20:49:51 -0800

    pages done

--- js/script.js
+++ js/script.js
@@ -1,20 +1,34 @@
 let triviaQ = [];
 let totalQuestions = 20;
-let btnText = document.getElementById('titleBtn');
+let inject = document.getElementById('inject');
 
-function loadQuestions() {
+function injectHTML(url, ){
     let xmlhttp = new XMLHttpRequest();
     let url = "";
 
-    if (diff == "esQ") {
-        url = "./data/data.json";
-    }
-    // This is our fail safe-----------------------------
-    if (diff == "haQ") {
-        url = "./data/data.json";
-    }
-    //-------------------------------------------
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            let myArr = JSON.parse(this.responseText);
+            if(url === "../mainPage.html"){
+                loadMainPage(myArr);
+            }else if(url === "../options.html"){
+                loadOptions(myArr);
+            }else if(url === "../instructions.html"){
+                loadInstructions(myArr);
+            }else if(url === "../gamePage.html"){
+                loadGamePage(myArr);
+            }
+        }
+    },
+    xmlhttp.open("GET", url, true);
+    xmlhttp.send();
+}
 
+let easyQ = [];
+let meQ = [];
+let godQ = [];
+function loadQuestions() {
+    let xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             let myArr = this.responseText //JSON.parse(this.responseText);
@@ -23,12 +37,39 @@ function loadQuestions() {
     };
     xmlhttp.open("GET", url, true);
     xmlhttp.send();
+    function allQuestions(info){
+        for(let i =0 ; i < easyQ.length;i++){
+            let nfo =[
+                "q" = info.esQ[i].q,
+                "a1" = info.esQ[i].a1,
+                "a2" = info.esQ[i].a2,
+                "a3" = info.esQ[i].a3,
+                "a4" = info.esQ[i].a4,
+                "cA" = info.esQ[i].cA
+            ]
+        }
+        for(let i =0; i < medQ.length;i++){
+            let nfo=[
+                "q" = info.esQ[i].q,
+                "a1" = info.medQ[i].a1,
+                "a2" = info.medQ[i].a2,
+                "a3" = info.medQ[i].a3,
+                "a4" = info.medQ[i].a4,
+                "cA" = info.medQ[i].cA
+            ]
+        }
+        for(let i =0; i < medQ.length;i++){
+            let nfo=[
+                "q" = info.gowQ[i].q,
+                "a1" = info.gowQ[i].a1,
+                "a2" = info.gowQ[i].a2,
+                "a3" = info.gowQ[i].a3,
+                "a4" = info.gowQ[i].a4,
+                "cA" = info.gowQ[i].cA
+            ]
+        }
+    }
 }
 
-function allQuestions(info){
-    btnText.addEventListener('click', function(e){
-        console.log(e);
-    });
-}
-
+injectHTML();
 loadQuestions();
